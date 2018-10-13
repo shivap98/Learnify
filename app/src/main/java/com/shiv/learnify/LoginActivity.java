a892c3eb1440ec858c0cc80ad7b9630985f61e14
kartikmittal33 <mittal38@purdue.edu>
2018-10-13 03:12:14 -0400

    Connected Firebase

--- app/src/main/java/com/shiv/learnify/LoginActivity.java
+++ app/src/main/java/com/shiv/learnify/LoginActivity.java
@@ -3,8 +3,14 @@ package com.shiv.learnify;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+
 public class LoginActivity extends AppCompatActivity
 {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -13,5 +19,14 @@ public class LoginActivity extends AppCompatActivity
         setContentView(R.layout.activity_login);
 
         String s = "shivisawesome";
+//        mAuth = FirebaseAuth.getInstance();
+
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference ref = firebaseDatabase.getReference("a");
+
+        ref.child("a").setValue("hey");
+
     }
+
+
 }
